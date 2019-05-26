a59b4e8400146f34bdf1115ad74e010699acdb35
Truvam <truvam@users.noreply.github.com>
2019-05-26 11:11:55 +0100

    Added pie chart to exploratory data analysis

--- exploratory_data_analysis.py
+++ exploratory_data_analysis.py
@@ -3,6 +3,19 @@ import seaborn as sns
 sns.set_style('dark')
 
 
+def plot_pre_processing(reviews_bf, reviews):
+    # Pie chart, where the slices will be ordered and plotted counter-clockwise:
+    labels = 'Before Pre-Proccessing', 'After Pre-Processing '
+    sizes = [len(reviews_bf), len(reviews_bf) - len(reviews)]
+
+    fig1, ax1 = plt.subplots()
+    ax1.pie(sizes, labels=labels, autopct='%1.1f%%', shadow=False,
+            startangle=90)
+    ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+    plt.title("Before and after Pre-Processing ratings")
+    plt.show()
+
+
 def plot_nratings_per_movie(ratings_mean_count):
     mean = int(round(ratings_mean_count['rating_counts'].mean(), 0))
     plt.figure(figsize=(8, 6))
